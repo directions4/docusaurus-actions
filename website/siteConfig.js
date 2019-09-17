d888bca1392e122175bfdd0d4ef726af0e66e6c5
directions4 <satoru@shikata.com>
2019-09-17 14:54:37 +0900

    update config

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,15 +23,13 @@ const users = [
 const siteConfig = {
   title: 'Docusaurus Test', // Title for your website.
   tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://directions4.github.io', // Your website URL
+  baseUrl: '/docusaurus-actions/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
-
-  // Used for publishing and more
-  projectName: 'docusaurus-site',
-  organizationName: 'facebook',
+  projectName: 'docusaurus-actions',  // The name of your GitHub project. Same as above.
+  organizationName: 'directions4', // Your GitHub username.
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
